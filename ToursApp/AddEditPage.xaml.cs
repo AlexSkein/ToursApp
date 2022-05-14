e1864e58a5365adb34e426897e4e8ba7683e256a
AlexSkein <AekseyKlubkov@yandex.ru>
2022-05-14 20:31:31 +0300

    Database part2

--- ToursApp/AddEditPage.xaml.cs
+++ ToursApp/AddEditPage.xaml.cs
@@ -20,16 +20,52 @@ namespace ToursApp
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selectedtHotel)
         {
             InitializeComponent();
+            if (selectedtHotel != null)
+            {
+                _currentHotel = selectedtHotel;
+            }
+            DataContext = _currentHotel;
             ComboCountries.ItemsSource = ToursBaseEntities.GetContext().Country.ToList();
 
         }
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
+            StringBuilder errors = new StringBuilder();
 
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля.");
+            if (_currentHotel.CountOfStars < 1 || _currentHotel.CountOfStars > 5)
+                errors.AppendLine("Количество звезд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentHotel.id == 0)
+                ToursBaseEntities.GetContext().Hotel.Add(_currentHotel);
+
+            try
+            {
+                ToursBaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("Сохранено.");
+                Manager.MainFrame.GoBack();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+            
         }
     }
 }
